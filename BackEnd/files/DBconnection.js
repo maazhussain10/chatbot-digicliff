9f412f129c268f7ec3df24633d2816ae196604bd
Dwabzy <jspragadeesh@gmail.com>
2021-06-10 23:06:52 +0530

    Snowpack Version
    Changed from Create-React-App to Snowpack

--- BackEnd/files/DBconnection.js
+++ BackEnd/files/DBconnection.js
@@ -1,19 +1,17 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-exports.DataBaseConnection = async (hostname,userName, password, databaseName) => {
-    console.log("GGGG",hostname,userName, password, databaseName)
-    const DBconnection = mysql.createConnection({
-        host: hostname,
-        user: userName,
-        password: password,
-        database: databaseName,
-    });
+exports.DataBaseConnection = async (hostname, userName, password, databaseName) => {
+  const DBconnection = mysql.createConnection({
+    host: hostname,
+    user: userName,
+    password: password,
+    database: databaseName,
+  });
 
-    DBconnection.connect(function (err) {
-        if (err) console.log("Knowledge Store Database connection unsuccessful!");
-        else console.log("Knowledge Store Database connected successfully");
-    });
+  DBconnection.connect(function (err) {
+    if (err) console.log("Knowledge Store Database connection unsuccessful!");
+    else console.log("Knowledge Store Database connected successfully");
+  });
 
-    return await DBconnection;
-
-}
+  return await DBconnection;
+};
