2a9abbbcea6400208b95d8fc5d7be11f01ca004e
Dwabzy <jspragadeesh@gmail.com>
2021-09-03 18:24:43 +0530

    Minor removal of files

--- BackEnd/configs/db.config.js
+++ BackEnd/configs/db.config.js
@@ -1,6 +1,6 @@
 const DB_HOST = "localhost";
-const DB_USERNAME = "root";
-const DB_PASSWORD = "Dwabzy";
+const DB_USERNAME = "";
+const DB_PASSWORD = "";
 const DB_NAME = "hospital";
 
 module.exports = { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME }
